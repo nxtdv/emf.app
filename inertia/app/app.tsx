6cd5025a3751a5d24a897a29428e7dd71d202add
nyt <nyt.git@gmail.com>
2025-08-12 17:35:02 +0200

    chore(app): update app title logic and default name

--- inertia/app/app.tsx
+++ inertia/app/app.tsx
@@ -7,12 +7,12 @@ import { createInertiaApp } from '@inertiajs/react'
 import { resolvePageComponent } from '@adonisjs/inertia/helpers'
 import { ThemeProvider } from '@/components/theme-provider'
 
-const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
+const appName = import.meta.env.VITE_APP_NAME || 'MXGR'
 
 createInertiaApp({
   progress: { color: '#5468FF' },
 
-  title: (title) => `${title} - ${appName}`,
+  title: () => appName,
 
   resolve: (name) => {
     return resolvePageComponent(`../pages/${name}.tsx`, import.meta.glob('../pages/**/*.tsx'))
