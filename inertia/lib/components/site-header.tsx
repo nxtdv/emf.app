7b4c884b494b048f598960e387e260d03f5fd5b8
nyt <nyt.git@gmail.com>
2025-08-12 17:33:34 +0200

    chore(site-header): update GitHub link to new repository URL

--- inertia/lib/components/site-header.tsx
+++ inertia/lib/components/site-header.tsx
@@ -20,7 +20,7 @@ export function SiteHeader({ title = 'Documents' }: SiteHeaderProps) {
         <div className="ml-auto flex items-center gap-2">
           <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
             <a
-              href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
+              href="https://github.com/nxtdv/emf.app"
               rel="noopener noreferrer"
               target="_blank"
               className="dark:text-foreground"
