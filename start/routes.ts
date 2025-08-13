a618a86418ede54dfd005339b1f153bb02b7bc5c
nyt <nyt.git@gmail.com>
2025-08-13 17:35:38 +0200

    chore(routes): add auth routes for login, logout, and registration

--- start/routes.ts
+++ start/routes.ts
@@ -7,5 +7,17 @@
 |
 */
 
+const LoginController = () => import('#controllers/login_controller')
+const LogoutController = () => import('#controllers/logout_controller')
+const RegisterController = () => import('#controllers/register_controller')
 import router from '@adonisjs/core/services/router'
-router.on('/').renderInertia('home')
+
+router.on('/').renderInertia('home').as('home')
+
+router.get('/login', [LoginController, 'render']).as('auth.login.render')
+router.post('/login', [LoginController, 'execute']).as('auth.login.execute')
+
+router.post('/logout', [LogoutController, 'execute']).as('auth.logout.execute')
+
+router.get('/register', [RegisterController, 'render']).as('auth.register.render')
+router.post('/register', [RegisterController, 'execute']).as('auth.register.execute')
